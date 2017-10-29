83019f04a3848b5e0900c126e59508b3863df331
GreenerEyes <annie.rowley19@gmail.com>
2017-10-29 16:51:59 -0600

    Basic tracking implemented

--- KinectJam/Form1.Designer.cs
+++ KinectJam/Form1.Designer.cs
@@ -28,12 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.rtbMessages = new System.Windows.Forms.RichTextBox();
+            this.video = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.video)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // rtbMessages
+            // 
+            this.rtbMessages.Location = new System.Drawing.Point(12, 498);
+            this.rtbMessages.Name = "rtbMessages";
+            this.rtbMessages.Size = new System.Drawing.Size(640, 83);
+            this.rtbMessages.TabIndex = 0;
+            this.rtbMessages.Text = "";
+            // 
+            // video
+            // 
+            this.video.Location = new System.Drawing.Point(12, 12);
+            this.video.Name = "video";
+            this.video.Size = new System.Drawing.Size(640, 480);
+            this.video.TabIndex = 1;
+            this.video.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(671, 593);
+            this.Controls.Add(this.video);
+            this.Controls.Add(this.rtbMessages);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.video)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.RichTextBox rtbMessages;
+        private System.Windows.Forms.PictureBox video;
     }
 }
 
--- KinectJam/Form1.cs
+++ KinectJam/Form1.cs
@@ -7,14 +7,204 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.Kinect;
+using Microsoft.Kinect.Toolkit;
+
+
+/*References
+ * https://code.msdn.microsoft.com/windowsdesktop/Beginning-Kinect-for-a198d400
+ * https://msdn.microsoft.com/en-us/library/jj131025.aspx
+ * */
 
 namespace KinectJam
 {
     public partial class Form1 : Form
     {
+        private KinectSensorChooser _chooser;
+        private KinectSensor _sensor;
+        private Skeleton[] _skeletonData;
+
+        private const float _renderWidth = 640.0f;
+        private const float _renderHeight = 480.0f;
+
+        private const double _jointThickness = 3;
+        private const double _bodyCenterThickness = 10;
+        private const double _clipBoundsThickness = 10;
+
+        //private readonly System.Windows.Media.Brush _centerPointBrush = System.Windows.Media.Brushes.Blue;
+        //private readonly System.Windows.Media.Brush _trackedJointBrush = System.Windows.Media.Brushes.Red;
+        //private readonly System.Windows.Media.Brush _inferredJointBrush = System.Windows.Media.Brushes.Yellow;
+
+        //private readonly System.Windows.Media.Pen _trackedBonePen = new System.Windows.Media.Pen(System.Windows.Media.Brushes.Green, 6);
+        //private readonly System.Windows.Media.Pen _inferredBonePen = new System.Windows.Media.Pen(System.Windows.Media.Brushes.Gray, 1);
+
+        private Graphics _graphics;
+        private Rectangle _rectangle = new Rectangle(10, 10, 10, 10);
+        private Pen _pen = new Pen(Brushes.Red, 6);
+        private Pen _borderPen = new Pen(Brushes.Black, 3.0f);
+
+
+
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            _chooser = new KinectSensorChooser();
+            _chooser.KinectChanged += ChooserSensorChanged;
+            _chooser.Start();
+
+            _graphics = video.CreateGraphics();
+        }
+
+        private void ChooserSensorChanged(object sender, KinectChangedEventArgs e)
+        {
+            KinectSensor oldSensor = e.OldSensor;
+            StopKinect(oldSensor);
+
+            _sensor = e.NewSensor;
+            if (_sensor == null)
+                return;
+
+            _sensor.SkeletonStream.Enable();
+            _sensor.ColorStream.Enable(ColorImageFormat.RgbResolution640x480Fps30);
+            _sensor.DepthStream.Enable(DepthImageFormat.Resolution640x480Fps30);
+
+            _skeletonData = new Skeleton[_sensor.SkeletonStream.FrameSkeletonArrayLength];
+            _sensor.SkeletonFrameReady += NewSensor_SkeletonFrameReady;
+
+            try
+            {
+                _sensor.Start();
+                rtbMessages.Text = "Kinect Started" + "\r";
+            }
+            catch (System.IO.IOException)
+            {
+                rtbMessages.Text = "Kinect Not Started" + "\r";
+                _chooser.TryResolveConflict();
+            }
+
+        }
+
+        private void NewSensor_SkeletonFrameReady(object sender, SkeletonFrameReadyEventArgs e)
+        {
+            using (SkeletonFrame skeletonFrame = e.OpenSkeletonFrame())
+            {
+                if (skeletonFrame != null && this._skeletonData != null)
+                {
+                    skeletonFrame.CopySkeletonDataTo(this._skeletonData);
+                    
+                }
+            }
+
+            Bitmap _bitmap = new Bitmap((int)_renderWidth, (int)_renderHeight);
+            using (_graphics = Graphics.FromImage(_bitmap))
+            {
+                DrawSkeletons();
+            }
+            video.Image = _bitmap;
+            
+
+        }
+
+        private void StopKinect(KinectSensor sensor)
+        {
+            if (sensor != null)
+            {
+                if (sensor.IsRunning)
+                {
+                    sensor.Stop();
+                    sensor.AudioSource.Stop();
+                }
+            }
+        }
+
+        private void DrawSkeletons()
+        {
+            foreach (Skeleton skeleton in this._skeletonData)
+            {
+                if (skeleton.TrackingState == SkeletonTrackingState.Tracked)
+                {
+                    DrawTrackedSkeletonJoints(skeleton.Joints);
+                }
+                else if (skeleton.TrackingState == SkeletonTrackingState.PositionOnly)
+                {
+                    DrawSkeletonPosition(skeleton.Position);
+                }
+            }
+        }
+
+        private void DrawTrackedSkeletonJoints(JointCollection jointCollection)
+        {
+            DrawBone(jointCollection[JointType.Head], jointCollection[JointType.ShoulderCenter]);
+            
+            DrawBone(jointCollection[JointType.ShoulderCenter], jointCollection[JointType.ShoulderLeft]);
+            DrawBone(jointCollection[JointType.ShoulderLeft], jointCollection[JointType.ElbowLeft]);
+            DrawBone(jointCollection[JointType.ElbowLeft], jointCollection[JointType.WristLeft]);
+            DrawBone(jointCollection[JointType.WristLeft], jointCollection[JointType.HandLeft]);
+
+            DrawBone(jointCollection[JointType.ShoulderCenter], jointCollection[JointType.ShoulderRight]);
+            DrawBone(jointCollection[JointType.ShoulderRight], jointCollection[JointType.ElbowRight]);
+            DrawBone(jointCollection[JointType.ElbowRight], jointCollection[JointType.WristRight]);
+            DrawBone(jointCollection[JointType.WristRight], jointCollection[JointType.HandRight]);
+
+            DrawBone(jointCollection[JointType.ShoulderCenter], jointCollection[JointType.Spine]);
+            DrawBone(jointCollection[JointType.Spine], jointCollection[JointType.HipCenter]);
+
+            DrawBone(jointCollection[JointType.HipCenter], jointCollection[JointType.HipLeft]);
+            DrawBone(jointCollection[JointType.HipLeft], jointCollection[JointType.KneeLeft]);
+            DrawBone(jointCollection[JointType.KneeLeft], jointCollection[JointType.AnkleLeft]);
+            DrawBone(jointCollection[JointType.AnkleLeft], jointCollection[JointType.FootLeft]);
+
+            DrawBone(jointCollection[JointType.HipCenter], jointCollection[JointType.HipRight]);
+            DrawBone(jointCollection[JointType.HipRight], jointCollection[JointType.KneeRight]);
+            DrawBone(jointCollection[JointType.KneeRight], jointCollection[JointType.AnkleRight]);
+            DrawBone(jointCollection[JointType.AnkleRight], jointCollection[JointType.FootRight]);
+
+        }
+
+        private void DrawBone(Joint jointFrom, Joint jointTo)
+        {
+            DepthImagePoint p1 = _sensor.CoordinateMapper.MapSkeletonPointToDepthPoint(jointFrom.Position, DepthImageFormat.Resolution640x480Fps30);
+            DepthImagePoint p2 = _sensor.CoordinateMapper.MapSkeletonPointToDepthPoint(jointTo.Position, DepthImageFormat.Resolution640x480Fps30);
+            if (!video.IsDisposed)
+                _graphics.DrawLine(_pen, p1.X, p1.Y, p2.X, p2.Y);
+        }
+
+        private void DrawSkeletonPosition(SkeletonPoint position)
+        {
+
+        }
+
+        //private void DrawBone(Joint jointFrom, Joint jointTo)
+        //{
+        //    if (jointFrom.TrackingState == JointTrackingState.NotTracked || jointTo.TrackingState == JointTrackingState.NotTracked)
+        //    {
+        //        return; // nothing to draw, one of the joints is not tracked
+        //    }
+
+        //    if (jointFrom.TrackingState == JointTrackingState.Inferred || jointTo.TrackingState == JointTrackingState.Inferred)
+        //    {
+        //        DrawNonTrackedBoneLine(jointFrom.Position, jointTo.Position);  // Draw thin lines if either one of the joints is inferred
+        //    }
+
+        //    if (jointFrom.TrackingState == JointTrackingState.Tracked && jointTo.TrackingState == JointTrackingState.Tracked)
+        //    {
+        //        DrawTrackedBoneLine(jointFrom.Position, jointTo.Position);  // Draw bold lines if the joints are both tracked
+        //    }
+        //}
+
+        private void DrawTrackedBoneLine(SkeletonPoint jointFromPosition, SkeletonPoint jointToPosition)
+        {
+            _graphics.DrawLine(_pen, jointFromPosition.X*100, jointFromPosition.Y*100, jointToPosition.X*100, jointFromPosition.Y*100);
+        }
+
+        private void DrawNonTrackedBoneLine(SkeletonPoint jointFromPosition, SkeletonPoint jointToPosition)
+        {
+
         }
     }
 }
